f9d4476907d7de4d4f9e1afb4e777151a7759808
Shai Guendelman <shaigue@rishon.csf.technion.ac.il>
2019-06-06 18:01:59 +0300

    mm

--- exp3_run.sh
+++ exp3_run.sh
@@ -2,7 +2,8 @@
 # a script for doing the RNN job
 
 # ./py-sbatch.sh main.py run-nb --allow-errors Part1_Sequence.ipynb
-./py-sbatch.sh main.py run-nb --allow-errors Part2_VAE.ipynb
+#./py-sbatch.sh main.py run-nb --allow-errors Part2_VAE.ipynb
 # command:
 # runipy ***notebook_name **out_name
 # ./py-sbatch.sh main.py runipy Part2_VAE.ipynb Part2_out
+./py-sbatch.sh main.py run-nb --allow-errors Part3_GAN.ipynb
